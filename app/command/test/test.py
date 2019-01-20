206fccb07a37c7882150cffc14256b18737cb75a
Mati Green <mati.green.13@gmail.com>
2019-01-20 20:55:14 +0200

    added togglemessages and dialogs

--- app/command/test/test.py
+++ app/command/test/test.py
@@ -1,12 +1,15 @@
 from app.component.dialogs.dialogs import dialogs
+from app.frame.body.body import body
 import const.error as error
 
-commands = [ 'a', 'append', 'load', 'l' ]
+commands = [ 'a', 'append', 'load', 'l', 'toggle-char', 'tgc', 'toggle-dialogs', 'tgd' ]
 
 __alias = {
     'append': [ 'append', 'a' ],
     'dialog': [ 'dialog', 'd' ],
-    'load'  : [ 'load', 'l' ]
+    'load'  : [ 'load',   'l' ],
+    'toggle-chat': [ 'toggle-chat', 'tgc' ],
+    'toggle-dialogs': [ 'toggle-dialogs', 'tgd' ]
 }
 
 def do_command(line, args):
@@ -26,3 +29,13 @@ def do_command(line, args):
                 dialogs.load_dialogs()
             else:
                 return error.command_bad_argument.format(argument=args[1])
+    elif args[0] in __alias['toggle-chat']:
+        if line not in __alias['toggle-chat']:
+            return error.command_has_no_arguments.format(command=args[0])
+        else:
+            dialogs.toggle_messages()
+    elif args[0] in __alias['toggle-dialogs']:
+        if line not in __alias['toggle-dialogs']:
+            return error.command_has_no_arguments.format(command=args[0])
+        else:
+            body.toggle_dialogs()
--- app/component/dialogs/dialogs.py
+++ app/component/dialogs/dialogs.py
@@ -5,21 +5,24 @@ import tg.action.dialog as tg
 class Dialogs(List):
     def __init__(self):
         super().__init__()
+        self.__maximized_messages = True
+        self.on_toggle = lambda: None
 
     def render(self, size, focus=False):
-        if not focus:
+        if not focus and len(self.body):
             self.body[self.focus_position].unmark_dialog()
         elif len(self.body):
             self.body[self.focus_position].mark_dialog()
         return super().render(size, focus)
 
     def append_dialog(self, dialog):
-        self.append_child(Dialog(dialog), 'end')
+        self.append_child(Dialog(dialog, self.__maximized_messages), 'end')
 
     def remove_dialog(self, dialog):
         self.remove_child(Dialog(dialog))
 
     def load_dialogs(self):
+        self.body.clear()
         self.__total_count, self.__dialogs = tg.get_gialogs()
         for dialog in self.__dialogs:
             self.append_dialog(dialog)
@@ -29,5 +32,26 @@ class Dialogs(List):
             return super().keypress(size, key)
         self.body[self.focus_position].unmark_dialog()
         return super().keypress(size, key)
+
+    def minimize_messages(self):
+        if not self.__maximized_messages:
+            return
+        for dialog in self.body:
+            dialog.minimize_messages()
+            self.__maximized_messages = False
+
+    def maximize_messages(self):
+        if self.__maximized_messages:
+            return
+        for dialog in self.body:
+            dialog.maximize_messages()
+            self.__maximized_messages = True
+
+    def toggle_messages(self):
+        self.on_toggle(not self.__maximized_messages)
+        if self.__maximized_messages:
+            self.minimize_messages()
+        else:
+            self.maximize_messages()
 
 dialogs = Dialogs()

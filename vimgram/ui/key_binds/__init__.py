9b9aa59e2e871806ee74869f995b580901e0f135
Mati Green <mati.green.13@gmail.com>
2019-02-13 17:38:13 +0200

    added key listener

--- vimgram/ui/key_binds/__init__.py
+++ vimgram/ui/key_binds/__init__.py
@@ -1,2 +1,25 @@
+import threading
+from . import normal
+
+class Keys:
+    def __init__(self):
+        self.keys = []
+        self.timeout = threading.Timer(0, lambda: 0)
+
+    def append_key(self, key):
+        self.keys.append(key)
+        self.timeout.cancel()
+        self.timeout = threading.Timer(2, self.process_keys)
+        self.timeout.start()
+
+    def process_keys(self):
+        if self.keys == []:
+            return
+        print(self.keys)
+        self.keys = []
+
+keys = Keys()
+
 def key_binds(key):
-    pass
+    global keys
+    keys.append_key(key)
